46ee4108c4fb288a3d2bdd1c9ab8f75197cc51d8
Gigi Ditz <donnaditz@gmail.com>
2016-04-01 15:08:54 -0700

    Modify Ping-pong

---
+++ README.md
@@ -0,0 +1,28 @@
+# Ping Pong Game
+
+#### Javascript Arrays & Looping Independent project - Week 3
+
+by Gigi Ditz
+
+## Description
+
+Create a web application that takes a number from a user and returns a range of numbers from 1 to the chosen number with the following exceptions:
+
+Numbers divisible by 3 are replaced with "ping"
+Numbers divisible by 5 are replaced with "pong"
+Numbers divisible by 15 are replaced with "pingpong"
+A user should be able to enter a new number and see new results over and over again.
+
+
+Technologies Used
+
+HTML
+CSS
+Javascript
+jQuery
+Bootstrap
+
+
+
+
+Copyright (c) 2016 Gigi Ditz
--- js/scripts.js
+++ js/scripts.js
@@ -4,18 +4,10 @@ var pingponggame = function(inputNumber){
   }  else {
     return false;
   }
-  // var pingponggame1 = function(inputNumber){
-  //   if (inputNum % 15 === 0)) {
-  //     return true;
-  //   }  else {
-  //     return false;
-  //   }
 };
-
 $(document).ready(function(){
   $("#pingpong form").submit(function(event){
     var inputNum= parseInt($("input#input1").val());
-    $("ul").empty();
 
     for (var i = 1; i <= inputNum; i += 1) {
       if (i % 15 === 0){
@@ -23,7 +15,7 @@ $(document).ready(function(){
       }
       else if (i % 3 === 0){
         $("ul").append("<li>ping</li>");
-        }
+      }
       else if (i % 5 === 0){
         $("ul").append("<li>pong</li>");
       }
@@ -31,8 +23,6 @@ $(document).ready(function(){
       $("ul").append("<li>" + i + "</li>");
       }
       event.preventDefault();
-console.log("result")
     }
-    });
-
+  });
 });
