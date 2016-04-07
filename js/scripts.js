97a16940f00a51bae4a53c67b3341242ed2b04ea
Gigi Ditz <donnaditz@gmail.com>
2016-04-07 16:52:05 -0700

    separate business logic from user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,36 @@
-var pingponggame = function(inputNumber){
-  if ((inputNum % 3 === 0) || (inputNum % 5 === 0) || (inputNum % 15 === 0)) {
-    return true;
-  }  else {
-    return false;
-  }
+// var pingPongGame = function(inputNumber){
+//   if ((inputNum % 3 === 0) || (inputNum % 5 === 0) || (inputNum % 15 === 0)) {
+//     return true;
+//   }  else {
+//     return false;
+//   }
+// };
+//----old script above--------//
+
+var PingPongGame = function(inputNum){
+
+  for (var i = 1; i <= inputNum; i += 1) {
+    if (i % 15 === 0){
+      $("ul").append("<li>pingpong</li>");
+    }
+    else if (i % 3 === 0){
+      $("ul").append("<li>ping</li>");
+    }
+    else if (i % 5 === 0){
+      $("ul").append("<li>pong</li>");
+    }
+    else {
+      $("ul").append("<li>" + i + "</li>");
+    }
+  };
 };
+
 $(document).ready(function(){
   $("#pingpong form").submit(function(event){
     var inputNum= parseInt($("input#input1").val());
-     $("ul").empty();
+    var result = PingPongGame(inputNum);
 
-    for (var i = 1; i <= inputNum; i += 1) {
-      if (i % 15 === 0){
-        $("ul").append("<li>pingpong</li>");
-      }
-      else if (i % 3 === 0){
-        $("ul").append("<li>ping</li>");
-      }
-      else if (i % 5 === 0){
-        $("ul").append("<li>pong</li>");
-      }
-      else {
-      $("ul").append("<li>" + i + "</li>");
-      }
-      event.preventDefault();
-    }
+    event.preventDefault();
+
   });
 });
